6d72a31be4bdc0f44b20f0e6e13697d3efe05268
Dr Flame <drflame707@gmail.com>
2024-04-13 00:01:26 +0500

    Scaffold is ready

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -1,5 +1,12 @@
 function HangmanDrawing() {
-    return <></>
+    return (
+        <div style={{position: 'relative'}}>
+            <div style={{height: '50px', width: '10px', background: 'black', position: 'absolute', top: 0, right: 0 }}/>
+            <div style={{height: '10px', width: '200px', background: 'black', marginLeft: '120px'}}/>
+            <div style={{height: '400px', width: '10px', background: 'black', marginLeft: '120px'}}/>
+            <div style={{height: '10px', width: '250px', backgroundColor: 'black'}}/>
+        </div>
+    )
 }
 
 export default HangmanDrawing;
