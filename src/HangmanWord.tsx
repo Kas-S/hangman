7b77a65a4cfe41603bc37228f89bd219dc1246be
Dr Flame <drflame707@gmail.com>
2024-04-13 11:49:03 +0500

    Base for frontend of showing word is ready

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,5 +1,26 @@
 function HangmanWord() {
-    return <></>
+    const word = "test"
+    const guessedLetters: string[] = []
+    return (
+        <div style={{
+            display: 'flex',
+            gap: '.25em',
+            fontSize: '6rem',
+            fontWeight: 'bold',
+            textTransform: 'uppercase',
+            fontFamily: 'monospace'
+        }}>
+            { word.split("").map((letter, i)=> (
+                <span style={{borderBottom: ".1em solid black"}} key={i}>
+                    <span style={{
+                        visibility: guessedLetters.includes(letter) ? "visible" : "hidden"
+                    }}>
+                        {letter}
+                    </span>
+                </span>
+            )) }
+        </div>
+    )
 }
 
 export default HangmanWord;
