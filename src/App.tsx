9b42382125f0e72749dbe7bed3987611ff6cafb6
Dr Flame <drflame707@gmail.com>
2024-04-13 14:22:45 +0500

    Project is finalized

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,9 @@ function App() {
   const [guessedLetters, setGuessedLetters] = useState<string[]>([])
   const incorrectLetters = guessedLetters.filter(letter => !wordToGuess.includes(letter))
 
+  const isLoser = incorrectLetters.length >= 6,
+        isWinner = wordToGuess.split("").every(letter => guessedLetters.includes(letter))
+
   const addGuessedLetter = useCallback((letter: string) => {
       if (guessedLetters.includes(letter)) return
       setGuessedLetters(currentLetters => [...currentLetters, letter])
@@ -36,11 +39,15 @@ function App() {
       margin: '0 auto',
       alignItems: 'center'
     }}>
-      <div style={{fontSize: '2rem', textAlign: 'center'}}>Lose Win</div>
+      <div style={{fontSize: '2rem', textAlign: 'center'}}>
+          {isWinner && "Win! Refresh to try again."}
+          {isLoser && "Lose! Nice try"}
+      </div>
       <HangmanDrawing numberOfWrongGuesses={ incorrectLetters.length }/>
       <HangmanWord guessedLetters={ guessedLetters } wordToGuess={ wordToGuess }/>
       <div style={{ alignSelf: "stretch" }}>
           <Keyboard
+              disabled={isWinner || isLoser}
               activeLetters={ guessedLetters.filter(letter => !wordToGuess.includes(letter)) }
               inactiveLetters={ incorrectLetters }
               addGuessedLetter={ addGuessedLetter }
--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -2,16 +2,28 @@ import './style.css'
 
 const ALPHABET: string[] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-function Keyboard() {
+interface KeyboardProps {
+    activeLetters: string[],
+    inactiveLetters: string[],
+    addGuessedLetter: (letter: string) => void,
+    disabled: boolean
+}
+
+function Keyboard({activeLetters, inactiveLetters, addGuessedLetter, disabled}: KeyboardProps) {
     return (
         <div style={{
             display: 'grid',
             gridTemplateColumns: 'repeat(auto-fit, minmax(75px, 1fr))',
             gap: '.5rem'
         }}>
-            { ALPHABET.map((key) => (
-                <button className="btn" key={key}>{key}</button>
-            )) }
+            { ALPHABET.map((key) => {
+                const isActive = activeLetters.includes(key),
+                      isInactive = inactiveLetters.includes(key)
+
+                return (
+                    <button disabled={isActive || isInactive || disabled} onClick={() => addGuessedLetter(key)} className={"btn" + (isActive ? " active": "") + (isInactive ? " inactive": "")} key={key}>{key}</button>
+                )
+            }) }
         </div>
     )
 }
